3503e78889bd5ae4a6cdf52366c487a2a4fc8b72
myungwoo-Y <auddn6676@naver.com>
2020-05-06 15:05:57 +0900

    implement disjointSet.cpp

---
+++ Algorithms Implementation/DisjointSet/src/disjointSet.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct DisjointSet{
+    vector<int> parent;
+    vector<int> counts;
+    DisjointSet(int size): parent(size), counts(size, 1){
+        for(int i = 0; i < parent.size(); i++)
+            parent[i] = i;
+    }
+    int find(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node] = find(parent[node]);
+    }
+    int merge(int a, int b){
+        int parentA = find(a), parentB = find(b);
+        if(parentA == parentB)
+            return counts[parentA];
+        else{
+            if(counts[parentB] > counts[parentA]){
+                counts[parentB] += counts[parentA];
+                parent[parentA] = parentB;
+                return counts[parentB];
+            }else{
+                counts[parentA] += counts[parentB];
+                parent[parentB] = parentA;
+                return counts[parentA];
+            }
+        }
+    }
+};

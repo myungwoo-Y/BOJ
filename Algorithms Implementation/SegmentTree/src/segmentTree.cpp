0a0cd236ff4e3dedc0ab926193251063674859b0
myungwoo-Y <auddn6676@naver.com>
2020-10-05 06:00:19 +0900

    add: 구조체로 된 세그먼트 트리 추가

--- Algorithms Implementation/SegmentTree/src/segmentTree.cpp
+++ Algorithms Implementation/SegmentTree/src/segmentTree.cpp
@@ -1,5 +1,44 @@
 // based by https://codeforces.com/blog/entry/18051
 // 초간단 반복문형 세그먼트 트리!
+// 이 트리는 트리 사이즈가 2의 지수승이 아니어도 동작한다.
+
+
+// 구조체 버전
+const int LimitArr = 1e6;
+struct SegTree{
+    int leafSize;
+    int t젼ree[LimitArr * 2];
+
+    SegTree(int size){
+        memset(tree, 0, LimitArr * 2);
+        leafSize = size;
+    }
+
+    void init() {
+        for (int i = leafSize - 1; i > 0; i--){
+            tree[i] = tree[i << 1] + tree[i << 1 | 1];
+        }
+    }
+
+    int query(int l, int r) {
+        int res = 0;
+        for (l += leafSize, r += leafSize; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) res += tree[l++];
+            if (r & 1) res += tree[--r];
+        }
+        return res;
+    }
+
+    void update(int pos, int val) {
+        tree[pos + leafSize] += val; // update
+        for (pos += leafSize; pos > 1; pos >>= 1) {
+            tree[pos >> 1] = tree[pos] + tree[pos ^ 1]; // update by rule
+        }
+    }
+};
+
+
+// 구조체 x 버
 const int TreeN = 1e5;
 int n;
 int tree[TreeN * 2];

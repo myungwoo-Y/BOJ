367a4697bf91fc56ad182e53a5570345783d7cd3
myungwoo-Y <auddn6676@naver.com>
2020-10-06 18:22:01 +0900

    add: 2차원 테이블에 대한 좌표값들 중 k개 고르는것 추가

--- Algorithms Implementation/Combination/src/combination.cpp
+++ Algorithms Implementation/Combination/src/combination.cpp
@@ -28,3 +28,44 @@ void makeCombination(int n, int k){
 
     cout << ans;
 }
+
+
+// 좌표에 대한 조합
+vector<vector<pii>> makeCombination(){
+    vector<pii> elements;
+    int k = 2;
+
+    for (int i = 1; i <= rowN; ++i) {
+        for (int j = 1; j <= colN; ++j) {
+            elements.push_back({i, j});
+        }
+    }
+
+    vector<int> combination;
+
+    for(int i=0; i<k; i++){
+        combination.push_back(1);
+    }
+
+    for(int i=0; i < elements.size() - k; i++){
+        combination.push_back(0);
+    }
+
+    vector<vector<pii>> ret;
+
+    sort(combination.begin(), combination.end());
+    do{
+        vector<pii> temp;
+        // 고른원소 사용
+        for(int i=0; i < combination.size(); i++){
+            if(combination[i] == 1){
+                temp.push_back(elements[i]);
+            }
+        }
+
+        ret.push_back(temp);
+
+    }while(next_permutation(combination.begin(), combination.end()));
+
+    return ret;
+}

76e3a5c1488bdbb2b7c9c3cb42b37ecc93e01a6c
myungwoo-Y <auddn6676@naver.com>
2021-11-10 06:11:48 +0900

    Permutation Sequence

---
+++ Math/LeetCode/PermutationSequence.cpp
@@ -0,0 +1,29 @@
+// Title : 60. Permutation Sequence
+
+class Solution {
+public:
+    
+    string getPermutation(int n, int k) {
+        string ans = "";
+        vector<int> nums = {1};
+        vector<int> facto = {1};
+
+        for (int i = 1; i < n; i++) {
+            facto.push_back(facto[i-1] * i);
+            nums.push_back(i+1);
+        }
+
+        k--;
+
+        for (int i = n-1; i >= 0; i--) {
+            int idx = k/facto[i];
+            k -= idx * facto[i];
+            ans += string(1, '0'+nums[idx]);
+            nums.erase(nums.begin()+idx);
+        }
+
+        return ans;
+     
+    
+    }
+};

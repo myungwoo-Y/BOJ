92075059d919053722fc88d435bc1af415b126e6
myungwoo-Y <auddn6676@naver.com>
2022-04-05 02:11:16 +0900

    1675. Minimize Deviation in Array

---
+++ PriorityQueue/LeetCode/MinimizeDeviationInArray.cpp
@@ -0,0 +1,55 @@
+// 1675. Minimize Deviation in Array
+
+class Solution {
+public:
+    priority_queue<int> q;
+    unordered_map<int, int> m;
+    int minimumDeviation(vector<int>& nums) {
+        int ret = INT_MAX, minNum = INT_MAX;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 != 0) {
+                nums[i] *= 2;
+            }
+        }
+
+        for (int num : nums) {
+            minNum = min(minNum, num);
+            if (m.find(num) == m.end()) {
+                m[num] = 1;
+            } else {
+                m[num]++;
+            }
+            q.push(num);
+        }
+
+        while (!q.empty()) {
+            int t = q.top();
+            q.pop();
+
+            m[t]--;
+
+            if (t == minNum && m[t] > 0) {
+                ret = 0;
+                break;
+            }
+
+            ret = min(ret, t - minNum);
+
+            if (t % 2 == 0) {
+                int newNum = t/2;
+                minNum = min(minNum, newNum);
+                q.push(newNum);
+                if (m.find(newNum) == m.end()) {
+                    m[newNum] = 1;
+                } else {
+                    m[newNum]++;
+                }
+            } else {
+                break;
+            }
+        }
+
+        return ret;
+    }
+};
